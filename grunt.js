5c13909b4cdb274c9e3002817ea55cfe88b5b57d
Simon Basset <simon.basset.pro@gmail.com>
2013-05-29 22:20:33 +0200

    update CV

--- grunt.js
+++ grunt.js
@@ -1,18 +1,18 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		mincss: {
-			all: {
-				src: ["stylesheets/foundation.css","stylesheets/foundicons.css","stylesheets/app.css"],
-				dest: "stylesheets/min.css"
-			}
-		},
-		clean: {
-			all: {
-				src:['stylesheets/min.css']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib');
-	grunt.registerTask('build', 'clean mincss');
-	grunt.registerTask('default', 'build');
+module.exports = function (grunt) {
+    grunt.initConfig({
+        mincss: {
+            all: {
+                src: ["stylesheets/foundation.css", "stylesheets/foundicons.css", "stylesheets/app.css"],
+                dest: "stylesheets/min.css"
+            }
+        },
+        clean: {
+            all: {
+                src: ['stylesheets/min.css']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib');
+    grunt.registerTask('build', 'clean mincss');
+    grunt.registerTask('default', 'build');
 };
