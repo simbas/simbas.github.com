0d57ec54f0798b54716441c5116c5f2f6329251f
Simon Basset <simon@macbook-de-simon.home>
2016-06-13 11:38:07 +0200

    add timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,5 +39,46 @@
 			});
 
 	});
-
 })(jQuery);
+
+
+jQuery(document).ready(function($){
+	var $timeline_block = $('.cd-timeline-block');
+
+	//hide timeline blocks which are outside the viewport
+	$timeline_block.each(function(){
+		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		}
+	});
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		$timeline_block.each(function(){
+			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			}
+		});
+
+	});
+
+
+			$('.skill-percent').each(function() {
+				var style = $(this).attr('style');
+				var reg = /width\s*:\s*(\d*)%/g;
+
+				var width = parseInt(reg.exec(style)[1]);
+				var rendered = false;
+				$(this).css({width: 0});
+				$(window).on('scroll', $.proxy(function () {
+				if(!rendered && $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.95) {
+					$(this).animate({
+						//finds the width from the data-percent attribute
+						width: width + '%'
+					}, 1000);
+					rendered = true;
+				}
+				// animate
+			}, this));
+			});
+});
